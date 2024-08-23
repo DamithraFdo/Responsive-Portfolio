848aad55a7cefdcf60f8b57ff0403d27f31c5dc1
DamithraFdo <damithrafdo@gmail.com>
2024-08-23 22:59:05 +0530

    add portfolio initial files to repo

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Robo Development","Hardware Designing","IoT Development","Embeded Systems","Electronics","Electricle","App Development"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
